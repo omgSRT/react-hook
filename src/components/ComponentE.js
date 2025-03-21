6bd9bd6f3d7e7a20afd9d5bf4ab1760c23bab7b0
omgSRT <thangckdt@gmail.com>
2025-03-21 15:00:50 +0700

    feat: useReducer with useContext

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,15 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import ComponentF from "./ComponentF";
-import { UserContext, ChannelContext } from "../App";
 
 function ComponentE() {
-  const userContext = useContext(UserContext);
-  const channelContext = useContext(ChannelContext);
 
   return (
     <div>
       <ComponentF />
-      {userContext} - {channelContext}
     </div>
   );
 }
--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import { UserContext, ChannelContext } from '../App'
+import React, { useContext } from "react";
+import { CountContext } from "../App";
+import { UserContext, ChannelContext } from "../App";
 
 function ComponentF() {
+  //   return (
+  //     <div>
+  //         <UserContext.Consumer>
+  //             {
+  //                 user => {
+  //                     return (
+  //                         <ChannelContext.Consumer>
+  //                             {
+  //                                 channel => {
+  //                                     return <div>User context value {user}, channel context value {channel}</div>
+  //                                 }
+  //                             }
+  //                         </ChannelContext.Consumer>
+  //                     )
+  //                 }
+  //             }
+  //         </UserContext.Consumer>
+  //     </div>
+  //   )
+  const countContext = useContext(CountContext);
   return (
     <div>
-        <UserContext.Consumer>
-            {
-                user => {
-                    return (
-                        <ChannelContext.Consumer>
-                            {
-                                channel => {
-                                    return <div>User context value {user}, channel context value {channel}</div>
-                                }
-                            }
-                        </ChannelContext.Consumer>
-                    )
-                }
-            }
-        </UserContext.Consumer>
+      ComponentF - {countContext.countState}
+      <button onClick={() => countContext.countDispatch("increment")}>
+        Increment
+      </button>
+      <button onClick={() => countContext.countDispatch("decrement")}>
+        Decrement
+      </button>
+      <button onClick={() => countContext.countDispatch("reset")}>Reset</button>
     </div>
-  )
+  );
 }
 
-export default ComponentF
+export default ComponentF;
